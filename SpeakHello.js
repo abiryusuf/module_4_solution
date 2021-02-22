e4a66f205fef3fb037cbb6214a6cd2bd0efaec83
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-21 23:12:29 -0500

    solution 4

---
+++ SpeakHello.js
@@ -0,0 +1,21 @@
+
+
+// var helloSpeaker = {}
+
+// var speakWord = "Hello";
+
+// function speak(name){
+//     console.log(speakWord + " " + name)
+// }
+
+(function(window){
+
+    var helloSpeaker = {}
+
+    var speakWord = "Hello";
+
+    helloSpeaker.speak = function(name){
+        console.log(speakWord + " " + name)
+}
+    window.helloSpeaker = helloSpeaker;
+})(window);
